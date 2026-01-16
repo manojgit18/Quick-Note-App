9138d87fa0038dd10bc6724e82a244e0a83be3bc
manojgit18 <manojmaverick96@gmail.com>
2026-01-17 01:01:09 +0530

    new app project

---
+++ BE/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/api/notes",(req,res)=>{
+    res.send("you got 5 notes")
+})
+
+app.listen(5001,()=>{
+    console.log("server started on port: 5001")
+})
+
