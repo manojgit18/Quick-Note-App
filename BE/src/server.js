c33af8dfc092ec9d8f1b0314f977d5e922d4629e
manojgit18 <manojmaverick96@gmail.com>
2026-01-18 02:34:38 +0530

    Meaningful commit message

--- BE/src/server.js
+++ BE/src/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const notesRoutes = require("./routes/notesRoutes");
+const connectDB = require("./config/db");
 const app = express();
-
+require("dotenv").config();
 
 app.use(express.json());
 
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("server started on port: 5001");
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port:${process.env.PORT}`);
 });
