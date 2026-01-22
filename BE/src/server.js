8eeb3c663247cd66691bc9cb359e9893a88b6cdc
manojgit18 <manojmaverick96@gmail.com>
2026-01-22 21:26:28 +0530

    Fix frontend dist path for Render deployment

--- BE/src/server.js
+++ BE/src/server.js
@@ -30,12 +30,10 @@ app.use("/api/notes", notesRoutes);
 
 /* Serve frontend in production */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../FE/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "../FE", "dist", "index.html"));
   });
 }
 
